376527462ae2ee0454cf4f3bc97748ae2fa8b31f
Andrew Lin <drewlin@me.com>
2020-09-14 23:45:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import './App.css';
 
+Amplify.configure(awsconfig);
+
 function App() {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const fetchLambda = async () => {
+      const lambdaData = await API.get('cclaDevApi', '/ping');
+      setResponse(lambdaData.success);
+    };
+
+    fetchLambda();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          data from lambda: {response}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
